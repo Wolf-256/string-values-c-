bb88b4bab0eb20dc571b3a79af3a72a73bf5cf3d
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2022-05-15 08:04:28 -0400

    Update maincode.cpp
    new line replaces end line

--- maincode.cpp
+++ maincode.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main() {
   //makes a string of the alphabet
   string alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  cout << "The length of the text string is: " << alphabetString.length() << endl;
-  cout << "first letter is "<< alphabetString[0]<< endl;
-  cout << "last letter is "<< alphabetString[25]<< endl;
+  cout << "The length of the text string is: " << alphabetString.length() << "\n";
+  cout << "first letter is "<< alphabetString[0]<< "\n";
+  cout << "last letter is "<< alphabetString[25]<< "\n";
   //array of alphabet
   //for each i value, a single letter string is set equal to array value
   string alphabetArray[26];
   for (int i = 0; i < alphabetString.length(); i++){
   alphabetArray[i]=alphabetString[i];
   }
-  cout << "first letter of array is  "<< alphabetArray[0]<< endl;
-  cout << "last letter of array is  "<< alphabetArray[25]<< endl;
+  cout << "first letter of array is  "<< alphabetArray[0]<< "\n";
+  cout << "last letter of array is  "<< alphabetArray[25]<< "\n";
   return 0;
 }
